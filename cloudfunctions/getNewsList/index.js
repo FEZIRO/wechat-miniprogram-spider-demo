2f0c180488c2b723870f4fa03afa97b0ca3e0706
FEZIRO <960838942@qq.com>
2019-08-29 13:39:41 +0800

    change some detail

--- cloudfunctions/getNewsList/index.js
+++ cloudfunctions/getNewsList/index.js
@@ -9,55 +9,54 @@ const SCHOOLCLUB_NEWSLIST_URL = BASE_URL + '/web/news/jingjingxiaoyuan/shetuanfe
 const UPCOMINGACTIVITY_NEWSLIST_URL = BASE_URL + '/web/news/jydt/huodongyugao/';
 const ACADEMIC_AFFAIRS_NEWSLIST_URL = 'http://jwc.xhsysu.edu.cn/';
 cloud.init();
+function getNewsList(p) {
+	return new Promise((resolve, reject) => {
+		axios.get(p).then(function (res) {
+			var $ = cheerio.load(res.data);
+			$('div.column_list ul ul li').each(function (i, element) {
+				var $element = $(element);
+				//匹配时间的正则表达式
+				var timePatt = /\d{4}\-\d{1,2}\-\d{1,2}\s*\d{2}\:\d{2}\:\d{2}/;
+				items.push({
+					imgPreviewUrl: BASE_URL + $element.find('img').attr('src'),
+					title: $element.find('h4 a').text(),
+					time: $element.find('p').first().text().match(timePatt),
+					thisNewsUrl: BASE_URL + $element.find('a').first().attr('href'),
+					target: 'normal'
+				})
+			})
+			return resolve(items);
+		}).catch(function (err) {
+			return reject(err);
+		});
+	})
+};
+
+function getAffairsAnnouncementList(p) {
+	return new Promise((resolve, reject) => {
+		axios.get(p).then(function (res) {
+			var $ = cheerio.load(res.data);
+			$('div.col-md-4.marginright .col-sm-12 .noside .li').each(function (i, element) {
+				var $element = $(element);
+				items.push({
+					id: i,
+					title: $element.find('.pull-left').text(),
+					time: $element.find('.pull-right').text(),
+					thisNewsUrl: ACADEMIC_AFFAIRS_NEWSLIST_URL + $element.find('a').attr('href'),
+					target: 'announcement'
+				});
+			});
+			return resolve(items);
+		}).catch(function (err) {
+			return reject(err);
+		});
+	})
+};
 
 // 云函数入口函数
 exports.main = async (event, context) => {
 	let thisPage;
 	let items = [];
-
-	function getNewsList(p) {
-		return new Promise((resolve, reject) => {
-			axios.get(p).then(function (res) {
-				var $ = cheerio.load(res.data);
-				$('div.column_list ul ul li').each(function (i, element) {
-					var $element = $(element);
-					//匹配时间的正则表达式
-					var timePatt = /\d{4}\-\d{1,2}\-\d{1,2}\s*\d{2}\:\d{2}\:\d{2}/;
-					items.push({
-						imgPreviewUrl: BASE_URL + $element.find('img').attr('src'),
-						title: $element.find('h4 a').text(),
-						time: $element.find('p').first().text().match(timePatt),
-						thisNewsUrl: BASE_URL + $element.find('a').first().attr('href'),
-						target: 'normal'
-					})
-				})
-				return resolve(items);
-			}).catch(function (err) {
-				return reject(err);
-			});
-		})
-	};
-
-	function getAffairsAnnouncementList(p) {
-		return new Promise((resolve, reject) => {
-			axios.get(p).then(function (res) {
-				var $ = cheerio.load(res.data);
-				$('div.col-md-4.marginright .col-sm-12 .noside .li').each(function (i, element) {
-					var $element = $(element);
-					items.push({
-						id: i,
-						title: $element.find('.pull-left').text(),
-						time: $element.find('.pull-right').text(),
-						thisNewsUrl: ACADEMIC_AFFAIRS_NEWSLIST_URL + $element.find('a').attr('href'),
-						target: 'announcement'
-					});
-				});
-				return resolve(items);
-			}).catch(function (err) {
-				return reject(err);
-			});
-		})
-	};
 
 	switch (event.newsTarget){
 			case 'mainNews':
